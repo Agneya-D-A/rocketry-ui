26c003d6b4630184a7834eb77ea800f847eb8f7d
Agneya D A <agneya181222da@gmail.com>
2025-08-20 13:13:20 +0530

    Fixed the prod backend as well

--- integrated_server.js
+++ integrated_server.js
@@ -2,14 +2,26 @@
 //AFTER SUCCESSFUL TESTING, ONLY THE VARIABLES WOULD BE INCREASED, THE LOGIC WOULD REMAIN THE SAME
 
 //Initializing paths and variables. Change these according to your requirements
-const frontendPath = "http://localhost:3000";
-const dbConnectionString = "mongodb://localhost:27017/rocketry-ui";
-const serialPortPath = "COM3/USB/VID_1A86&PID_7523/6&22A3D4F&0&2";
-const baudRate = 9600;
+const serialPortPath = process.env.SERIAL_PORT_PATH;
+const baudRate = parseInt(process.env.BAUD_RATE);
+const frontendPath = process.env.FRONTEND_PATH;
+const fs = require('fs');
+require('dotenv').config();
+const REACT_APP_CENTRE_X = parseFloat(process.env.REACT_APP_CENTRE_X);
+const REACT_APP_CENTRE_Y = parseFloat(process.env.REACT_APP_CENTRE_Y);
 
 //This is required for some cross origin request handling. 
 //Request is denied if this isn't present
 const cors = require('cors');
+
+//Setup CSV Logging
+const csvFilePath = process.env.CSV_FILE_PATH;
+const HEADERS = ["Vx", "Vy", "Vz","V", "Ax", "Ay", "Az","A", "altitude", "temperature", "pressure","Ox","Oy","Oz","timeStamp", "timeMilliSeconds","positionX","positionY","distanceTraversed"];
+const EXPECTED_COLUMNS = HEADERS.length;
+
+if (!fs.existsSync(csvFilePath)) {
+  fs.writeFileSync(csvFilePath, HEADERS.join(",") + "\n");
+}
 
 //create a server and initialize socket.io to send data from the server
 const express = require('express');
@@ -35,23 +47,23 @@ const io = require('socket.io')(server, {
 
 //Connect to MongoDB
 //import mongoose for MongoDB operations
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-//import SensorData model, a MongoDB schema model
-const SensorData = require('./src/models/SensorData');
+// //import SensorData model, a MongoDB schema model
+// const SensorData = require('./src/models/SensorData');
 
 //function to connect to MongoDB database
-const connectToDatabsase = async () =>{
-  let connection_string = dbConnectionString;
-  try {
-      await mongoose.connect(connection_string);
-      console.log('Connected to MongoDB via Mongoose!');
-  }
-  catch(error){
-      console.log('Error connecting to MongoDB:', error);
-  }
-}
-connectToDatabsase();
+// const connectToDatabsase = async () =>{
+//   let connection_string = dbConnectionString;
+//   try {
+//       await mongoose.connect(connection_string);
+//       console.log('Connected to MongoDB via Mongoose!');
+//   }
+//   catch(error){
+//       console.log('Error connecting to MongoDB:', error);
+//   }
+// }
+// connectToDatabsase();
 
 //////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -101,25 +113,49 @@ const handleSerialData = (serialArray) =>{
             Vx: serialArray[0],
             Vy: serialArray[1],
             Vz: serialArray[2],
-            V: vectorMagnitude(serialArray[0],serialArray[1],serialArray[2]).toFixed(5) //rounds to 5 decimal places
+            V: parseFloat(vectorMagnitude(serialArray[0],serialArray[1],serialArray[2])).toFixed(5)
         },
         acceleration: {
             Ax: serialArray[3],
             Ay: serialArray[4],
             Az: serialArray[5],
-            A: vectorMagnitude(serialArray[3],serialArray[4],serialArray[5]).toFixed(5)
+            A: parseFloat(vectorMagnitude(serialArray[3],serialArray[4],serialArray[5])).toFixed(5)
         },
         altitude: serialArray[6],
         temperature: serialArray[7],
         pressure: serialArray[8],
+        orientation: {
+            Ox: serialArray[9],
+            Oy: serialArray[10],
+            Oz: serialArray[11]
+        },
         timeStamp: time,
-        timeMilliSeconds: timeMilliSeconds
+        timeMilliSeconds: timeMilliSeconds,
+        position: {
+            x: serialArray[12],
+            y: serialArray[13],
+            distance: Math.sqrt((serialArray[12]- REACT_APP_CENTRE_X)**2 + (serialArray[13]- REACT_APP_CENTRE_Y)**2)
+        }
     }
-
     //create a new databse model object using the node and spreading it
-    const point = new SensorData({ ...node });
-    //save it to the database
-    point.save();
+    try {
+        let values = serialArray.slice(0,3);
+        values.push(node.velocities.V);
+        values.push(serialArray.slice(3,6));
+        values.push(node.acceleration.A);
+        values.push(serialArray.slice(6,12));
+        values.push([time, timeMilliSeconds]);
+        values.push([node.position.x, node.position.y, node.position.distance]);
+        
+        const csvLine = `${values.join(",")}\n`;
+
+        fs.appendFile(csvFilePath, csvLine, (err) => {
+        if (err) console.error("CSV Logging Error:", err);
+        });
+    } 
+    catch (err) {
+        console.error("Error processing data for CSV:", err);
+    }
 
     //emit the data through socket.io to the frontend
     io.emit('new-data',JSON.stringify(node));
@@ -129,7 +165,7 @@ const handleSerialData = (serialArray) =>{
 /////////////////////////////////////////////////////////////////////////////////////////////
 
 //Assigning which port the server looks for responses in
-const port_number = 3001;
+const port_number = parseInt(process.env.BACKEND_PORT);
 server.listen(port_number, 'localhost',() => {
   console.log(`Server listening on port ${port_number}`);
 });
